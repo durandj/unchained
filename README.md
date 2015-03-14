8cb140dcd686775392c0acc8f8b86d93ae88e61d
James Durand <james.durand@alumni.msoe.edu>
2015-03-13 20:45:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Django Unchained
+
+A set of tools for working with Django and getting moving faster
+
+###### Notes
+
+About the name, I had to.
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as file_handle:
+	LONG_DESCRIPTION = file_handle.read()
+
+with open('requirements.txt', 'r') as file_handle:
+	DEPENDENCIES = file_handle.readlines()
+
+setup(
+	name                 = 'unchained',
+	version              = '1.0.0',
+	url                  = 'https://github.com/durandj/unchained',
+	license              = 'MIT',
+	description          = 'Tools for getting Django setup quickly',
+	long_description     = LONG_DESCRIPTION,
+	author               = 'James Durand',
+	author_email         = 'james.durand@alumni.msoe.edu',
+	packages             = find_packages(),
+	include_package_data = True,
+	package_data         = [],
+	install_requires     = DEPENDENCIES,
+)
+
